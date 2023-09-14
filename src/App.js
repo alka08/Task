a05467ef8a75153012b77670cfb960678a029466
Alka Mishra <alka71807@gmail.com>
2023-09-13 20:30:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data,setdata]=useState([]);
+  const fetchdata=()=>{
+    return axios.get('https://jsonplaceholder.typicode.com/posts/1/comments').then((response)=> setdata(response.data));
+  }
+useEffect(
+  ()=>{
+    fetchdata();
+  }
+)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> calling the api </h1>
+       
+            <table style={{ width: 1200 }}>
+                <thead>
+                    <tr>
+                    <th>Name</th>
+                        {data.map((data1, headID) =>
+                            <th key={headID} >{data1.name}</th>)
+                        }
+                    </tr>
+                    <tr>
+                    <th>Email</th>
+                        {data.map((data1, email) =>
+                            <th key={email} >{data1.email}</th>)
+                        }
+                    </tr>
+                    <tr>
+                      <th>Body</th>
+                        {data.map((data1, body) =>
+                            <th key={body} >{data1.body}</th>)
+                        }
+                    </tr>
+                </thead>
+               
+            </table>
+        
     </div>
   );
 }
